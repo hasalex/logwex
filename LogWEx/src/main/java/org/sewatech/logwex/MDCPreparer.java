dec6c641e713c478c93e96a58ff5ef1f8ff8b0d6
Alexis Hassler <alexis@sewatech.org>
2010-08-10 20:52:41 +0200

    Updated copyright

--- LogWEx/src/main/java/org/sewatech/logwex/MDCPreparer.java
+++ LogWEx/src/main/java/org/sewatech/logwex/MDCPreparer.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2010 Sewatech
+ *
+ * This file is part of LogWEx.
+ *
+ * LogWEx is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published bythe Free Software Foundation, in version 3 of the
+ * License.
+ *
+ * LogWEx is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with LogWEx.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
 package org.sewatech.logwex;
 
 import org.apache.commons.beanutils.PropertyUtils;
@@ -5,7 +24,7 @@ import org.apache.log4j.Logger;
 import org.apache.log4j.MDC;
 
 import javax.servlet.ServletContext;
-import javax.servlet.http.HttpServletRequest; 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.beans.PropertyDescriptor;
 import java.security.Principal;
--- LogWEx/src/main/java/org/sewatech/logwex/MDCPreparerFilter.java
+++ LogWEx/src/main/java/org/sewatech/logwex/MDCPreparerFilter.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2010 Sewatech
+ *
+ * This file is part of LogWEx.
+ *
+ * LogWEx is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published bythe Free Software Foundation, in version 3 of the
+ * License.
+ *
+ * LogWEx is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with LogWEx.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
 package org.sewatech.logwex;
 
 import org.apache.log4j.Logger;
@@ -12,7 +31,7 @@ import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 /**
- * Created : 28 juil. 2010 
+ * Created : 28 juil. 2010
  *
  * @author Alexis Hassler
  * @since 1.0
